591fca31a4f44dfc883d9380ee275cd6a58ee265
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-06-08 18:27:32 +0530

    Time: 12 ms (77.30%), Space: 6.4 MB (94.96%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,19 +4,21 @@ public:
         int n=word1.size();
         int m=word2.size();
         
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        vector<int> prev(m+1,0),cur(m+1,0);
         
-        for(auto i=0;i<=n;i++) dp[i][0]=i;
-        for(auto i=0;i<=m;i++) dp[0][i]=i;
+       // for(auto i=0;i<=n;i++) dp[i][0]=i;
+        for(auto i=0;i<=m;i++) prev[i]=i;
         
         for(auto i=1;i<=n;i++){
+            cur[0]=i;
             for(auto j=1;j<=m;j++){
                 if(word1[i-1]==word2[j-1])
-                    dp[i][j]= dp[i-1][j-1];
+                    cur[j]= prev[j-1];
                 else
-                    dp[i][j]= min(1 +dp[i-1][j],min(1+dp[i][j-1],1+dp[i-1][j-1]));
+                    cur[j]= 1 + min(prev[j],min(cur[j-1],prev[j-1]));
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
