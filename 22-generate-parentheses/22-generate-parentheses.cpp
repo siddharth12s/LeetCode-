36ba23d81496e404498a94dff822e1d4495c4791
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-06-19 22:42:23 +0530

    Time: 3 ms (85.58%), Space: 13.9 MB (38.66%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -13,21 +13,6 @@ public:
             generate(s + ")",open,close+1,n,ans);
         }
     }
-//     void generate(int n, int open, int close,string s, vector<string>& ans)
-// {
-
-//     if (open == n && close == n) {
-//         ans.push_back(s);
-//         return;
-//     }
-//     if (open < n) {
-//         generate(n, open + 1, close, s + "(",ans);
-//     }
-
-//     if (close < open) {
-//         generate(n, open, close + 1, s + ")", ans);
-//     }
-// }
     vector<string> generateParenthesis(int n) {
      vector<string> ans;
     generate("", 0, 0, n, ans);
