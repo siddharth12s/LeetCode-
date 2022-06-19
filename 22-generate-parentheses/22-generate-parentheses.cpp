219eef124a4dd1d595653e8711ff0e36a445693e
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-06-19 22:42:10 +0530

    Time: 3 ms (85.58%), Space: 13.8 MB (47.94%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-//     vector<string> ans;
-//     void generate(string s,int i,int open,int close,int n){
-//         if(open==n and close==n){
-//             ans.push_back(s);
-//             return;
-//         } 
+    void generate(string s,int open,int close,int n,vector<string> &ans){
+        if(open==n and close==n){
+            ans.push_back(s);
+            return;
+        } 
         
-//         if(open<n){
-//             s += '(';
-//             generate(s,i+1,open+1,close,n);
-//         }
-//         if(open>close){
-//             s += ')';
-//             generate(s,i+1,open,close+1,n);
-//         }
+        if(open<n){
+            generate(s + "(",open+1,close,n,ans);
+        }
+        if(open>close){
+            generate(s + ")",open,close+1,n,ans);
+        }
+    }
+//     void generate(int n, int open, int close,string s, vector<string>& ans)
+// {
+
+//     if (open == n && close == n) {
+//         ans.push_back(s);
+//         return;
 //     }
-    void generate(int n, int open, int close,string s, vector<string>& ans)
-{
+//     if (open < n) {
+//         generate(n, open + 1, close, s + "(",ans);
+//     }
 
-    if (open == n && close == n) {
-        ans.push_back(s);
-        return;
-    }
-    if (open < n) {
-        generate(n, open + 1, close, s + "(",ans);
-    }
-
-    if (close < open) {
-        generate(n, open, close + 1, s + ")", ans);
-    }
-}
+//     if (close < open) {
+//         generate(n, open, close + 1, s + ")", ans);
+//     }
+// }
     vector<string> generateParenthesis(int n) {
      vector<string> ans;
-    generate(n, 0, 0, "", ans);
+    generate("", 0, 0, n, ans);
         return ans;
     }
 };
