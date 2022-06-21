377be824282cc51342441ed3efae11b4860f715d
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-06-21 22:08:43 +0530

    Time: 137 ms (47.94%), Space: 53.6 MB (19.84%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -5,11 +5,11 @@ public:
         unordered_map<int,int> m;
         int n=nums.size();
         
-        for(auto i=0;i<n;i++){
-            m[nums[i]]++;
+        for(auto i : nums){
+            m[i]++;
         }
-        for(auto i=m.begin();i!=m.end();i++){
-            if(i->second>1)
+        for(auto i : m){
+            if(i.second>1)
                 return true;
         }
         return false;
