573a12f8f23b1296bbcd1c062919b4c867dac73d
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-07-01 19:06:21 +0530

    Time: 398 ms (9.86%), Space: 69 MB (21.97%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>> graph(n+1);
+        vector<int> t(n+1,0);
         for(auto el : trust){
-            graph[el[0]].push_back(el[1]);
-        }
-        int ans=-1;
-        for(auto i=1;i<=n;i++){
-            if(graph[i].size()==0)
-                ans = i;
+            t[el[0]]--;
+            t[el[1]]++;
         }
         for(auto i=1;i<=n;i++){
-            if(i==ans)
-                continue;
-            
-            if(find(graph[i].begin(),graph[i].end(),ans)==graph[i].end()){
-                ans=-1;
-                break;
-            }
+            if(t[i]==n-1)
+                return i;
         }
-        return ans;
+        return -1;
     }
 };
