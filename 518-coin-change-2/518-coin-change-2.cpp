2e1412cd0935fe18bdd65d385633c00588b7fd13
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-07-04 14:41:02 +0530

    Time: 49 ms (40.15%), Space: 7.2 MB (79.43%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-
     int change(int amount, vector<int>& coins) {
-            int n=coins.size();
-            vector<vector<int>> dp(n,vector<int> (amount+1,0));
-            sort(coins.begin(),coins.end());
-            for(auto i=0;i<=amount;i++){
-                if(i%coins[0]==0)
-                    dp[0][i]=1;
-                else
-                    dp[0][i]=0;
-            }
+        int n = coins.size();
+        sort(coins.begin(),coins.end());
         
-            for(auto i=1;i<n;i++){
-                for(auto j=0;j<=amount;j++){
-                    int nottake= dp[i-1][j];
-                    int take=0;
-                    if(coins[i]<=j)
-                        take = dp[i][j-coins[i]];
-                    
-                    dp[i][j]=take + nottake;
-                }
+        vector<int> dp(amount+1,0);
+        dp[0]=1;
+        for(auto c : coins){
+            for(auto i=1;i<=amount;i++){
+                if(i-c>=0)
+                    dp[i] += dp[i-c];
             }
-        return dp[n-1][amount];
+        }
+        for(auto i=0;i<=amount;i++){
+            cout<<dp[i]<<" ";
+        }
+        return dp[amount];
     }
 };
