99bc86801b72d06b98defc9fa6911805af504688
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-08-13 22:18:30 +0530

    Time: 27 ms (84.92%), Space: 13.7 MB (87.51%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    
-    void dfs(vector<int> graph[],vector<int> & vis,int i){
+    int find(int v, int parent[]){
+        if(parent[v]==v)    return v;
         
-        vis[i]=1;
-        
-        for(auto x : graph[i]){
-            if(!vis[x])
-                dfs(graph,vis,x);
-        }
+        return parent[v]=find(parent[v],parent);
     }
     
-    
+    void Union(int a ,int b, int parent[]){
+        a = find(a,parent);
+        b = find(b,parent);
+        
+        if(a!=b)
+            parent[b]=a;
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
        
         int n = isConnected.size();
-        vector<int> graph[n];
         
+        int parent[n];
+        for(auto i=0;i<n;i++)   parent[i]=i;
+        
+        
+        int ans=0;
         for(auto i=0;i<n;i++){
-            for(auto j=0;j<n;j++){
-                if(isConnected[i][j])
-                    graph[i].push_back(j);
+            for(auto j=i+1;j<n;j++){
+                if(isConnected[i][j])   Union(i,j,parent);
             }
         }
         
-        int ans=0;
-        vector<int> vis(n,0);
         for(auto i=0;i<n;i++){
-            if(!vis[i])
-                ans++;
-                dfs(graph,vis,i);
+            if(parent[i]==i)    ans++;
         }
         
         return ans;
