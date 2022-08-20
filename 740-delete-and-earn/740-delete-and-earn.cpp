2819aec09d84321717452661a47c178441c02155
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-08-20 17:03:42 +0530

    Time: 17 ms (75.09%), Space: 11.9 MB (85.20%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int f(vector<int>& freq,int i,vector<int>& dp){
-        if(i<=0) return 0; 
-        
-        if(dp[i]!=-1)   return dp[i];
-        int take = i*freq[i] + f(freq,i-2,dp);
-        int nottake = f(freq,i-1,dp);
-        
-        return dp[i]=max(nottake,take);
-    }
+
     int deleteAndEarn(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
         vector<int> freq(nums[n-1]+1,0);
         for(auto i=0;i<n;i++)   freq[nums[i]]++;
-        vector<int> dp(nums[n-1]+1,-1);
-        return f(freq,nums[n-1],dp);
+        vector<int> dp(nums[n-1]+1);
+        dp[0]=0;
+        for(auto i=1;i<nums[n-1]+1;i++){
+            int take = i*freq[i];
+            if(i-2>=0)  take+=dp[i-2];
+            int nottake = dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[nums[n-1]];;
     }
 };
