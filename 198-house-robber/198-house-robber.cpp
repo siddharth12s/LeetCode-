9569abff7bc168a388892563504dea286f8cc520
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-08-20 14:53:39 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -11,7 +11,14 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(auto i=1;i<n;i++){
+            int pick = nums[i];
+            if(i-2>=0)  pick+=dp[i-2];
+            int notpick = dp[i-1];
+            dp[i]=max(pick,notpick);
+        }
+        return dp[n-1];
     }
 };
