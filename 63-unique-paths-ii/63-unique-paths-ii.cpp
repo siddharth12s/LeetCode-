cb8c758d12665b9ce90e79dd735041a52fc0500a
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-08-31 15:24:35 +0530

    Time: 6 ms (47.22%), Space: 7.8 MB (45.16%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -24,7 +24,17 @@ public:
         
         
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,a,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        
+        dp[0][1]=1;
+        
+        for(auto i=1;i<=m;i++){
+            for(auto j=1;j<=n;j++){
+                if(a[i-1][j-1]==1) dp[i][j]=0;
+                else
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
     }
 };
