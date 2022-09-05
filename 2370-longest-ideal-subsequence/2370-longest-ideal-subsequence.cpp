b79c4fc46ced4724dbfa58ec2aa20b5db2261877
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-09-05 03:45:20 -0700

    Time: 125 ms (69.16%), Space: 10.4 MB (59.68%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int f(string &s, int i,int k, char dum,vector<vector<int>> &dp){
-        if(i==s.length())
-            return 0;
-        
-        if(dp[i][dum]!=-1) return dp[i][dum];
-        
-        int pick=0;
-        if(abs(int(s[i])-int(dum))<=k or dum=='#')
-            pick = 1+f(s,i+1,k,s[i],dp);
-        int notpick = f(s,i+1,k,dum,dp);
-        return dp[i][dum]=max(pick,notpick);
-    }
+
     int longestIdealString(string s, int k) {
-        vector<vector<int>> dp(s.length()+1,vector<int>(150,-1));
-        return f(s,0,k,'#',dp);
+        vector<int> dp(26,0);
+        
+        for(auto &ch : s){
+            int i = ch-'a';
+            dp[i]++;
+            
+            for(auto j=max(0,i-k);j<= min(25,i+k);j++){
+                if(j!=i)
+                    dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+        
+        int ans=0;
+        for(auto i=0;i<26;i++){
+            ans=max(ans,dp[i]);
+        }
+        return ans;
     }
 };
