5f7a2c5e72e57b2801f7c3b29be64b826a143a6b
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-09-21 18:37:50 +0530

    Time: 16 ms (41.22%), Space: 11.2 MB (73.76%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
-    static bool comp(pair<int,int> p1,pair<int,int> p2){
-        if(p1.second==p2.second)
-            return p1.first>p2.first;
-        
-        return p1.second<p2.second;
-    }
-    vector<int> frequencySort(vector<int>& nums) {
-        map<int,int> m;
-        vector<pair<int,int>> temp;
-        
-        for(auto x : nums){
-            m[x]++;
-        }
-        
-        for(auto x : m){
-            temp.push_back({x.first,x.second});
-        }
-        
-        sort(temp.begin(),temp.end(),comp);
-        // nums.clear();
-        int k=0;
-        for(auto x : temp){
-            for(auto j=0;j<x.second;j++){
-                nums[k]=x.first;
-                k++;
-            }
-        }
-        return nums;
+
+    vector<int> frequencySort(vector<int>& A) {
+         unordered_map<int, int> count;
+        for (int a: A)
+            count[a]++;
+        sort(begin(A), end(A), [&](int a, int b) {
+            return count[a] == count[b] ? a > b : count[a] < count[b];
+        });
+        return A;
     }
 };
