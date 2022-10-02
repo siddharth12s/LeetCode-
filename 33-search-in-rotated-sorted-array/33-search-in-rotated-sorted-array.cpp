d17b7152aa459be382c68529b239f14458b4aca2
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-10-02 17:37:53 +0530

    Time: 7 ms (59.07%), Space: 11.1 MB (28.99%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -5,30 +5,21 @@ public:
     
     int search(vector<int>& nums, int target) 
     {
-         int l = 0, r = nums.size()-1;
-            while(l <= r)
-            {
-                int mid = (r - l)/2 + l;
-                double comparator = nums[mid];
-                // Checking if both target and nums[mid] are on same side.
-                if((target < nums[0]) && (nums[mid] < nums[0]) || (target >= nums[0]) && (nums[mid] >= nums[0]))
-                    comparator = nums[mid];
-                else
-                {
-                    // Trying to figure out where nums[mid] is and making comparator as -INF or INF
-                    if(target <nums[0])
-                        comparator = -INFINITY;
-                    else 
-                        comparator = INFINITY;
-
-                }
-                if(target == comparator) return mid;
-                if(target > comparator)            
-                    l = mid+1;            
-                else
-                    r = mid-1;
-
-            }
-            return -1;
+         int lo = 0, hi = nums.size();
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        
+        double num = (nums[mid] < nums[0]) == (target < nums[0])
+                   ? nums[mid]
+                   : target < nums[0] ? -INFINITY : INFINITY;
+                   
+        if (num < target)
+            lo = mid + 1;
+        else if (num > target)
+            hi = mid;
+        else
+            return mid;
+    }
+    return -1;
     }
 };
