07c5d3c51fd2620abc5c19af51a088a87416ef05
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-10-02 17:37:29 +0530

    Time: 7 ms (59.07%), Space: 11 MB (74.84%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,61 +1,34 @@
 class Solution {
 public:
     
-    int BinarySearch(vector<int>& nums, int target, int start, int end) {
-        int l=start, r=end;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] > target){
-                r = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
-        }
-        return -1;
-    }
-    
-    int findMin(vector<int>& nums) 
-    {
-        int n = nums.size();
-        int start = 0, end = n-1;
-        if(nums[start] < nums[end])
-        {
-            return 0;
-        }
-        while(start <= end)
-        {
-            int mid = start + (end - start)/2;
-            int next = (mid+1)%n;
-            int prev = (mid + n - 1)%n;
-            if(nums[mid] <= nums[next] && nums[mid] <= nums[prev])
-            {
-                return mid;
-            }
-            if(nums[mid] <= nums[end])
-            {
-                end = mid - 1;
-            }
-            else if(nums[start] <= nums[mid])
-            {
-                start = mid + 1;
-            }
-        }
-        return -1;
-    }
+  
     
     int search(vector<int>& nums, int target) 
     {
-        int n = nums.size();
-        int min_index = findMin(nums);
-        int a1 = BinarySearch(nums , target, 0, min_index - 1);
-        int a2 = BinarySearch(nums , target, min_index, n-1);
-        if(a1 == -1)
-            return a2;
-        else
-            return a1;
+         int l = 0, r = nums.size()-1;
+            while(l <= r)
+            {
+                int mid = (r - l)/2 + l;
+                double comparator = nums[mid];
+                // Checking if both target and nums[mid] are on same side.
+                if((target < nums[0]) && (nums[mid] < nums[0]) || (target >= nums[0]) && (nums[mid] >= nums[0]))
+                    comparator = nums[mid];
+                else
+                {
+                    // Trying to figure out where nums[mid] is and making comparator as -INF or INF
+                    if(target <nums[0])
+                        comparator = -INFINITY;
+                    else 
+                        comparator = INFINITY;
+
+                }
+                if(target == comparator) return mid;
+                if(target > comparator)            
+                    l = mid+1;            
+                else
+                    r = mid-1;
+
+            }
+            return -1;
     }
 };
