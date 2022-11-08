c4aca04b0f92611ee49a9253d71720aebf58793f
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-11-08 17:28:48 +0530

    Time: 2 ms (84.38%), Space: 6.2 MB (82.16%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans;
+        
+        for(auto i=0;i<s.size();i++){
+            ans.push_back(s[i]);
+            
+            while(ans.size() and (ans.back()==s[i+1]+32 or ans.back()==s[i+1]-32)){
+                ans.pop_back();
+                i++;
+            }
+        }
+        return ans;
+    }
+};
