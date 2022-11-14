ca65072569aa36507d7708995c79d52f0b4c131e
siddharth12s <103488748+siddharth12s@users.noreply.github.com>
2022-11-14 20:14:47 +0530

    Time: 11 ms (72.11%), Space: 13.2 MB (54.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>> &grid,int i,int j,int &ans){
-        
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        int m =grid.size();
-        int n = grid[0].size();
-        
-        int delr[] = {-1,0,1,0};
-        int delc[] = {0,-1,0,1};
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                auto p = q.front();
-                q.pop();
-
-                for(auto a=0;a<4;a++){
-                    int x = p.first + delr[a];
-                    int y = p.second + delc[a];
-
-                    if(x>=0 and x<m and y>=0 and y<n and grid[x][y]==1){
-                        grid[x][y]=2;
-                        q.push({x,y});
-                    }
-                }
-            }
-            ans++;
-        } 
-    }
+   
     int orangesRotting(vector<vector<int>>& grid) {
 
         int m = grid.size();
