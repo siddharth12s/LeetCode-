8cee74de6cb3b2ffd5b1b0ab16fe302cfc37f4fb
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-09-21 16:23:09 +0530

    Time: 3 ms (95.99%), Space: 11.7 MB (54.62%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        // int len = s.size();
-        // string ans = "";
 
+        //creating lps
+        string reversedString = s;
+        reverse(reversedString.begin(),reversedString.end());
 
-        // int start = 0;
-        // int end = len-1;
-        // int idx = 0;
-        // while(start<=end){
-        //     string temp = "";
-        //     if(s[start]!=s[end] and start<end){
-        //         temp += s[end];
-        //         temp += s[end];
-        //         ans.insert(idx,temp);
-        //         end--;
-        //     }else if(s[start]==s[end] and start==end){
-        //         temp+= s[end];
-        //         ans.insert(idx,temp);
-        //         end--;
-        //     }else{
-        //         temp+=s[end];
-        //         temp+=s[end];
-        //         ans.insert(idx,temp);
-        //         start++;
-        //         end--;
-        //     }
-        //     idx++;
-        // }
-        // return ans;
-                string reversedString = s;
-        // Reverse the original string
-        reverse(reversedString.begin(), reversedString.end());
+        string temp = s + "#" + reversedString;
+        int prevlps=0,i=1;
+        vector<int> lps(temp.size(),0);
+        // cout<<temp;
 
-        // Iterate through the string to find the longest palindromic prefix
-        for (int i = 0; i < s.size(); ++i) {
-            // memcmp to avoid creating substrings
-            if (!memcmp(s.c_str(), reversedString.c_str() + i, s.size() - i)) {
-                // Append the necessary part to make the palindrome
-                return reversedString.substr(0, i) + s;
+        while(i<temp.size()){
+            while(prevlps>0 and temp[prevlps]!=temp[i]){
+                prevlps = lps[prevlps-1];
             }
+            if(temp[i]==temp[prevlps]){
+                prevlps++;
+            }
+            lps[i]=prevlps;
+            i++;
         }
-        // Fallback case, append the whole reversed string to the original
-        // string
-        return reversedString + s;
-        
+
+        int palindromeLength = lps.back();
+
+        string suffix = reversedString.substr(0,s.size()-palindromeLength);
+        return suffix + s;
     }
 };
