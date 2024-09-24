c4a96bec11833ebf52dd9ddf4abe66c0617a8698
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-09-24 12:38:18 +0530

    Time: 58 ms (97.3%), Space: 72.6 MB (66.28%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,45 +1,78 @@
+class TrieNode{
+public:
+    TrieNode* child[26];
+    bool isLeaf=false;
 
+    TrieNode(){
+        for(auto i=0;i<26;i++){
+            child[i]=NULL;
+        }
+    }
+};
+
+class Trie{
+    TrieNode* root;
+public:
+    Trie(){
+        root = new TrieNode();
+    }
+
+    void insert(string key){
+
+        TrieNode* curr = root;
+
+        for(char ch : key){
+            int idx = ch-'a';
+
+            if(curr->child[idx]==NULL){
+                TrieNode* newNode = new TrieNode();
+
+                curr->child[idx]=newNode;
+            }
+
+            curr = curr->child[idx];
+        }
+
+        curr->isLeaf=true;
+    }
+
+    string search_prefix(string key){
+        TrieNode* curr = root;
+
+        for(int i=0;i<key.size();i++){
+            int idx = key[i]-'a';
+
+            if(curr->child[idx]==NULL)
+                return key;
+
+            curr = curr->child[idx];
+            if(curr->isLeaf){
+                return key.substr(0,i+1);
+            }
+
+        }
+
+        return key;
+    }
+};
 class Solution {
 public:
-    int longestprefix(string &s1, string &s2){
-        int m = s1.size();
-        int n = s2.size();
-
-        int i=0,j=0;
-        int len = 0;
-        while(i<m and j<n){
-            if(s1[i]==s2[j]){
-                i++;
-                j++;
-                len++;
-            }else{
-                break;
-            }
-        }
-
-        return len==0?INT_MAX:len;
-    }
     string replaceWords(vector<string>& dict, string sentence) {
-        stringstream str(sentence);
-        string word;
+        
+        Trie trie;
 
-        string ans = "";
-        while(str>>word){
-            int len = 0;
-            int minlen = INT_MAX;
-            for(auto x : dict){
-                len = longestprefix(word,x);
-                if(len==x.size())
-                    minlen = min(minlen,len);
-            }
-            if(minlen==INT_MAX){
-                ans += word;
-            }else{
-                ans += word.substr(0,minlen);
-            }
-            ans+= " ";
+        for(auto x : dict){
+            trie.insert(x);
         }
-        ans = string(ans.begin(),ans.end()-1);
+
+        stringstream st(sentence);
+        string word;
+        string ans = "";
+        while(st>>word){
+            ans += trie.search_prefix(word);
+            ans += " ";
+        }
+        ans.pop_back();
         return ans;
     }
 };
