bae3d8660759704c99cbd1c824e35384488e6fa5
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-09-25 12:42:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+
+        for(auto x : stones){
+            pq.push(x);
+        }
+
+        while(pq.size()>1){
+            int heaviest = pq.top();
+            pq.pop();
+            int second_heaviest = pq.top();
+            pq.pop();
+
+            if(second_heaviest<heaviest){
+                pq.push(heaviest-second_heaviest);
+            }
+        }
+
+        return pq.top();
+    }
+};
