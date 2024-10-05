2dc55d12480d87c4c55484e68b3539302e227904
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-05 18:57:31 +0530

    Time: 14 ms (5.73%), Space: 13.3 MB (31.5%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
+    int count_ones(vector<int> &m){
+        int start=0,end=m.size()-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(m[mid]==0){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         // vector<int> m(mat.size(),0);
         priority_queue<pair<int,int> ,vector<pair<int,int>>> pq;
 
         for(auto i=0;i<mat.size();i++){
-            int cnt=0;
-            for(auto y : mat[i]){
-                if(y==1)
-                    cnt++;
-            }
+            int cnt=count_ones(mat[i]);
             pq.push({cnt,i});
             if(pq.size()>k)
                 pq.pop();
