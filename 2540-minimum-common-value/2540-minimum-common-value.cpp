1f6fefe17942477255a5df0969dd00c07dedf9dc
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-06 17:38:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i=0,j=0;
-
-        while(i<nums1.size() and j<nums2.size()){
-            if(nums1[i]==nums2[j])
-                return nums1[i];
-            else if(nums1[i]<nums2[j]){
-                i++;
+    int binary_search(vector<int> &v, int target){
+        int start = 0, end = v.size()-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(v[mid]==target){
+                return v[mid];
+            }else if (v[mid]>target){
+                end = mid;
             }else{
-                j++;
+                start = mid+1;
             }
         }
-
         return -1;
+    }
+
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int ans = -1;
+        
+        if(nums2.size()>nums1.size()){
+            return getCommon(nums2,nums1);
+        }
+
+        for(auto x : nums2){
+            ans = binary_search(nums1,x);
+            if(ans!=-1)
+                break;
+        }
+
+        return ans;
     }
 };
