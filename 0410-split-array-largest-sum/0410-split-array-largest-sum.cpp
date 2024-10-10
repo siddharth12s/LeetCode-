ebc8fff4ca7245d5d462106764bcc53b001dcae0
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-10 12:35:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool possible(vector<int> &v, int k, int check_sum){
+        int cnt=1;
+        int sum=0;
+
+        for(auto x : v){
+            sum += x;
+            if(sum>=check_sum){
+                sum = x;
+                cnt++;
+                if(cnt>k)
+                    return false;
+            }
+        }
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int start =  *min_element(nums.begin(),nums.end());
+        int end = 0;
+        for(auto x : nums)  end+=x;
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(possible(nums,k,mid)){
+                end=mid;
+            }else{
+                start=mid+1;
+            }
+        }
+
+        return start-1;
+    }
+};
