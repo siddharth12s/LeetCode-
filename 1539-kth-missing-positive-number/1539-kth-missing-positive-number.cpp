14c80073de282482297d14b5584cb61f50f7605c
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-10 15:55:57 +0530

    Time: 7 ms (27.96%), Space: 12.4 MB (58.56%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -3,23 +3,13 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         // int maxi = *max_element(arr.begin(),arr.end());
 
-        unordered_set<int> s;
         for(auto x : arr){
-            s.insert(x);
-        }
-        
-        int ans=1;
-        for(auto i=1;i<=1000000;i++){
-            if(s.count(i)==0){
-                k--;
-            }
-
-            if(k==0){
-                ans=i;
+            if(x<=k)
+                k++;
+            else
                 break;
-            }
         }
 
-        return ans;
+        return k;
     }
 };
