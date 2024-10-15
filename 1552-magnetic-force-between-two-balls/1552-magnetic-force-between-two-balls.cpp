0f5361094ec9e2d9467e39553d943b3bd55a4ea8
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-15 07:48:01 +0530

    Time: 133 ms (23.93%), Space: 61.8 MB (55.16%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool feasible(vector<int> &v,int m,int mid){
+        int cnt = 1,last = v[0];
+        for(auto i=1;i<v.size();i++){
+                if(v[i]-last>=mid){
+                    last=v[i];
+                    cnt++;
+                }
+
+                if(cnt>=m){
+                    return true;
+                }
+        }
+        return false;
+    }
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        
+        int max_elem = *max_element(position.begin(),position.end());
+        int min_elem = *min_element(position.begin(),position.end());
+
+        int start = 1,end=max_elem-min_elem;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if (!feasible(position,m,mid)){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return start-1;
+    }
+};
