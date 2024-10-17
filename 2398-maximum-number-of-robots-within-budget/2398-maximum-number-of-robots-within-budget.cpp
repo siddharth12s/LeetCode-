ea5b6a6b77f2e29adb53d68aa2417d5b8fcae38b
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-17 22:11:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -1,118 +1,57 @@
-// class Solution {
-// public:
-//     bool isFeasible(vector<int> &c, vector<int> &r, int k, long long mid){
-//         long long sum = 0;
-//         long long start=0,end=0;
-//         long long maxi = (long long)c[0];
-//         deque<long long> dq;
-//         while(end<c.size()){
-//             sum += r[end];
-//             // if dq.size() is equal to mid(window which we are trying to find out)
-//             // if(!dq.empty() and dq.front()==end-mid) dq.pop_front();
-//             while(!dq.empty() and c[dq.back()] < c[end]){
-//                 dq.pop_back();
-//             }
-//             dq.push_back(end);
-
-//             if(end-start+1==mid){
-//                 maxi = c[dq.front()];
-
-//                 long long check = maxi + mid*sum;
-
-//                 if(check<=k)    return true;
-
-//                 sum-=r[start];
-//                 if(maxi==c[start]){
-//                     dq.pop_front();
-//                 }
-//                 start++;
-//             }  
-//             end++;
-//         }
-
-//         return false;
-//     }
-//     int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-//         long long start = 1;
-//         long long end = chargeTimes.size();
-//         long long ans =0;
-//         while(start<=end){
-//             auto mid = start+(end-start)/2;
-//             if(isFeasible(chargeTimes,runningCosts,budget,mid)){
-//                 ans=mid;
-//                 start=mid+1;
-//             }else{
-//                 end=mid-1;
-//             }
-//         }
-
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-    bool isValid(int k, vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        int n = chargeTimes.size();
-        
-        int i=0;
-        int j=0;
-        
+    bool isFeasible(vector<int> &c, vector<int> &r, long long k, long long mid) {
         long long sum = 0;
-        int maxi = 0;
-        
-        list<int> l;
-        
-        while(j<n) {
-            // calculate sum of running costs till we hit the window size
-            sum+=runningCosts[j];
+        long long start = 0, end = 0;
+        deque<int> dq;
+        long long maxi = (long long)c[0];
 
-            // to maintain maximum at the begining of the list
-            while(l.size() > 0 and l.back() < chargeTimes[j]) l.pop_back();
-            
-            l.push_back(chargeTimes[j]);
-            
-            if(j-i+1 == k) {
-                // when we hit the window size of k running robots
-                
-                maxi = l.front(); // maximum is at the front of the list
-                
-                long long b = maxi + k*sum;
-                // Cost of running k consecutive robots
-                // if in budget return true
-                if(b <= budget) return true;
+        while(end<c.size()){
+            sum += r[end];
 
-                // else shift the window by 1
-                sum-=runningCosts[i];
-                if(maxi == chargeTimes[i]) {
-                    l.pop_front();
-                }
-                i++;
+            while(!dq.empty() and c[dq.back()]<=c[end]){
+                dq.pop_back();
             }
-            
-            j++;
+            dq.push_back(end);
+
+            if(end-start+1>mid){
+            }
+
+            if(end-start+1==mid){
+                maxi = c[dq.front()];
+                long long check = maxi + mid*sum;
+
+                if(check<=k)
+                    return true;
+
+                sum-=r[start];
+                if(maxi==c[start]){
+                    dq.pop_front();
+                }
+                start++;
+            }
+            end++;
         }
-        
-        return false;
-        
+
+        return false;        
     }
 
     int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        int s = 1;
-        int e = chargeTimes.size();
-        
+        int start = 1;
+        int end = chargeTimes.size();
         int ans = 0;
-        
-        while(s<=e) {
-            int k = s+(e-s)/2;
-            
-            if(isValid(k, chargeTimes, runningCosts, budget)) {
-                ans = k;
-                s=k+1;
+
+        // Binary search over the number of robots (window size)
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isFeasible(chargeTimes, runningCosts, budget, mid)) {
+                ans = mid; // If feasible, update answer
+                start = mid + 1; // Try a larger window size
+            } else {
+                end = mid - 1; // Try a smaller window size
             }
-            else e = k-1;
         }
-        
-        return ans;
+
+        return ans; // Return the maximum number of robots that can run within the budget
     }
 };
