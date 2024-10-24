147f0a62fe8fad10c7c76236b8afffe0c290ffcf
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-10-24 23:33:58 +0530

    Time: 0 ms (100%), Space: 7.4 MB (52.34%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    void fun(string &s, int i){
-        if(i==1){
-            s+='0';
-            return;
-        }
-        fun(s,i-1);
-
-        string temp = "";
-        for(auto ch : s){
-            if(ch=='1'){
-                temp+="10";
-            }else{
-                temp+="01";
-            }
-        }
-        s=temp;
-    }
     int kthGrammar(int n, int k) {
-        string s;
-        fun(s,n);
-        cout<<s;
-        return s[k-1]-'0';
+        if(n==1){
+            if(k==1)
+                return 0;
+            else
+                return 1;
+        }
+        if(k%2) return kthGrammar(n-1,(k+1)/2);
+        return kthGrammar(n-1,k/2)?0:1;
     }
 };
