ca0c34b9a6cd0c0317887d5cae1270b5f656f1e6
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2024-11-03 13:53:21 +0530

    Time: 0 ms (100%), Space: 9.1 MB (43.48%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        priority_queue<int> max_heap;
-        for(auto i=0;i<numOnes;i++){
-            max_heap.push(1);
-        }
-        for(auto i=0;i<numZeros;i++){
-            max_heap.push(0);
-        }
-        for(auto i=0;i<numNegOnes;i++){
-            max_heap.push(-1);
-        }
+        if(k<=numOnes)
+            return k;
+        
+        if(k<=numOnes+numZeros)
+            return numOnes;
 
-        int ans = 0;
-        while(k--){
-            ans += max_heap.top();
-            max_heap.pop();
-        }
-
-        return ans;
+        return numOnes - (k-numOnes - numZeros);
     }
 };
