a7f0022311a20b629e1980db8f2e97158eb099de
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2025-01-13 23:48:29 +0530

    Time: 27 ms (58.87%), Space: 31.9 MB (86.69%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char,int> m1,m2;
+        vector<int> v(26,0);
 
         int ans = s.length();
         for(auto ch : s){
-            m1[ch]++;
-            if(m1.find(ch)!=m1.end()){
-                if(m1[ch]==3){
-                    ans -= 2;
-                    // m1.erase(ch);
-                    m1[ch]-=2;
-                }
+            v[ch-'a']++;
+            if(v[ch-'a']==3){
+                ans -= 2;
+                v[ch-'a']-=2;
             }
-            cout<<m1[ch]<<endl;
+            // cout<<v[ch]<<endl;
         }
 
         return ans;
