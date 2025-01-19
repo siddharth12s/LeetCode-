fcae0f8d190fc58fa65ba2a7b96d45e10e18f0b8
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2025-01-19 23:47:57 +0530

    Time: 0 ms (100%), Space: 27.4 MB (18.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,25 +3,22 @@ public:
     int trap(vector<int>& height) {
         int ans = 0;
 
-        vector<int> mxl(height.size(),0),mxr(height.size(),0);
+        vector<int> max_at_left(height.size(),0), max_at_right(height.size(),0);
 
-        mxl[0] = height[0];
-        mxr[height.size()-1] = height[height.size()-1];
-
-        for(auto i=1;i<height.size();i++){
-            mxl[i]=max(mxl[i-1],height[i]);
-        }
-        for(int i=height.size()-2;i>=0;i--){
-            mxr[i]=max(mxr[i+1],height[i]);
+        int l =-1,r=-1;
+        int n= height.size();
+        for(auto i=0;i<n;i++){
+            max_at_left[i] = max(l,height[i]);
+            l=max(l,height[i]);
         }
 
-        vector<int> water;
-        for(auto i=0;i<height.size();i++){
-            water.push_back(min(mxl[i],mxr[i])-height[i]);
+        for(auto i=n-1;i>=0;i--){
+            max_at_right[i]=max(r,height[i]);
+            r = max(r,height[i]);
         }
 
-        for(auto x : water){
-            ans += x;
+        for(auto i=0;i<n;i++){
+            ans += min(max_at_left[i],max_at_right[i]) - height[i];
         }
 
         return ans;
