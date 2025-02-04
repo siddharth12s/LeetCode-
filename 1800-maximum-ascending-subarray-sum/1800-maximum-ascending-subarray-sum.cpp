4087147b49a87729f1b99ff0d62ecb1a5b3122b6
Siddharth Shanker <103488748+siddharth12s@users.noreply.github.com>
2025-02-04 12:06:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int prev=nums[0];
+        int max_sum = prev;
+        int curr_sum = prev;
+
+        for(auto i=1;i<nums.size();i++){
+            if(nums[i]<prev){
+                curr_sum=nums[i];
+                prev=nums[i];
+                cout<<curr_sum<<endl;
+                continue;
+            }
+            prev=nums[i];
+            curr_sum += nums[i];
+            max_sum = max(max_sum,curr_sum);
+        }
+
+        return max_sum;
+    }
+};
